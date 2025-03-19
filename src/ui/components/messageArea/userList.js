bc40fd8b954a031189349ce41878785bf79d17b9
kanika <misskaniisun@gmail.com>
2025-03-19 13:57:57 -0400

    message user search and changes

--- src/ui/components/messageArea/userList.js
+++ src/ui/components/messageArea/userList.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../../../supabaseClient";
 import "./messages.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 const UserList = ({ setReceiver }) => {
   const [users, setUsers] = useState([]);
-  const loggedInUserId = localStorage.getItem("userId"); 
+  const loggedInUserId = localStorage.getItem("userId");
+  const [searchTerm, setSearchTerm] = useState("");
 
   // get users in db besides user thats logged in
   useEffect(() => {
@@ -14,23 +16,38 @@ const UserList = ({ setReceiver }) => {
         .select("userID, firstName")
         .neq("userID", loggedInUserId);
 
-        console.log("Logged in User ID:", loggedInUserId);
-  
+      console.log("Logged in User ID:", loggedInUserId);
+
       if (!error) {
         setUsers(data);
-        console.log("Fetched users:", data); 
+        console.log("Fetched users:", data);
       }
     };
     fetchUsers();
   }, [loggedInUserId]);
-  
 
-  
+  // user search filter
+  const filteredUsers = users.filter((user) =>
+    user.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     // display users in db
     <div className="user-list">
       <h3>Chat with:</h3>
-      {users.map((user) => (
+      {/* search input */}
+      <div className="user-search-box">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="user-search-input"
+        />
+      </div>
+
+      {/* search display */}
+      {filteredUsers.map((user) => (
         <div
           key={user.userID}
           className="user-item"
